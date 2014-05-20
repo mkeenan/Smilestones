2bc66fef29a760186a3b9649cd67baf28b5a39e3
Mary Keenan <marykeenan@Marys-MacBook-Pro.local>
2014-05-19 23:38:32 -0700

    procompiled assets locally

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -81,9 +81,9 @@ SmilestonesApp::Application.configure do
   config.paperclip_defaults = {
     :storage => :s3,
     :s3_credentials => {
-      :bucket => ENV['S3_BUCKET_NAME'],
-      :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
-      :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']
+      :bucket => ENV['marywdi'],
+      :access_key_id => ENV['AKIAJRVTCTZTLEHPPNDA'],
+      :secret_access_key => ENV['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']
     }
   }
 end
